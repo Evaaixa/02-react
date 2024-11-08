a79e151f444446883842b1c08c6704032e914c13
Eva Aixa <evaaixahm@gmail.com>
2024-11-08 11:15:46 +0100

    Archivos de clase

---
+++ src/components/UsersList.jsx
@@ -0,0 +1,30 @@
+import { useFetchData } from "../hooks/useFetchData"
+
+export const UsersList = ({url}) => {
+  const { data, loading } = useFetchData({url})
+
+  return (
+    <>
+      <ul>
+  
+        { loading ? <p>Cargando...</p> :
+        url === 'users' 
+        ?
+          data.map(user =>
+              <li key={user.id}>
+              <p>Nombre: {user.name} Email: {user.email}</p>
+              <span>Teléfono: {user.phone}</span>
+              </li>) 
+        :   
+          data.map(comment => 
+              <li key={comment.id}>
+              <p>Nombre: {comment.name}</p>
+              <span>Comentario: {comment.body}</span> <br/>
+              <span>Email: {comment.email} </span>
+              </li>)}
+    </ul>
+    
+    </>
+
+  )
+}
